7a7c1b66b148cd8817c875144918433694ab8442
Juampi <juampimarino@gmail.com>
2022-05-12 00:25:06 -0300

    ejercicio-terminado-juampi

--- 01-Ejercicios/index.js
+++ 01-Ejercicios/index.js
@@ -3,22 +3,42 @@
 // Si los números ingresados son iguales, debe retorar el string "Son iguales"
 
 function determinarMayor(num1, num2) {
-  // Armar acá el cuerpo de la función
+  if (num1 > num2) {
+    return num1;
+  } else {
+    if (num1 < num2) {
+      return num2;
+    } else {
+      return "Son iguales";
+    }
+  }
 }
+
+// Armar acá el cuerpo de la función
 
 // Implementar la función parImpar, la cual debe recibir por parámetros un número entero.
 // Retornar true... si el número es par.
 // Retornar false... si el número es impar
 
 function parImpar(num) {
-  // Armar acá el cuerpo de la función
+  if (num % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
+// Armar acá el cuerpo de la función
 
 // Implementar la función esNumero, la cual debe recibir por parámetros un valor cualquiera.
 // Retornar true... si el valor recibido es un número.
 // Retornar false... si el valor recibido NO es un número
 
 function esNumero(num) {
+  if (isNaN(num)) {
+    return false;
+  } else {
+    return true;
+  }
   // Armar acá el cuerpo de la función
 }
 
